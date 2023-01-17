5323e9c24e0c851b9dc93cc5f980a7aea74b03e4
Hossein Douzandeh <Douzandeh.k@email.come>
2023-01-17 09:57:57 +0330

    Points event

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-// Functions
-function classSwitcher(){
-    slides.forEach(slide => slide.classList.remove("active"))
-    points.forEach(point => point.classList.remove("active"))
-    slides[active].classList.add("active")
-    points[active].classList.add("active")
-}
-let goNext = () => {
-    active = (active == slides.length - 1) ? 0 : active + 1
-    classSwitcher()
-}
 // variables
-let active = 0
-let timer = 2000
+let active = 0;
+let timer = 3000;
 
-let slidshow = document.querySelector(".slideshow")
-let slides =document.querySelectorAll(".slide")
-let points = document.querySelectorAll(".points > span")
-let prev = document.querySelector(".prev")
-let next = document.querySelector(".next")
+let slidshow = document.querySelector(".slideshow");
+let slides = document.querySelectorAll(".slide");
+let points = document.querySelectorAll(".points > span");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+
+// Functions
+function classSwitcher() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  points.forEach((point) => point.classList.remove("active"));
+  slides[active].classList.add("active");
+  points[active].classList.add("active");
+}
+
+let goNext = () => {
+  active = active == slides.length - 1 ? 0 : active + 1;
+  classSwitcher();
+};
 // setInterval
-setInterval(goNext , timer)
+setInterval(goNext, timer);
+
+//  points event
+points.forEach((point, index) => {
+  point.addEventListener("click", (e) => {
+    active = index;
+    classSwitcher();
+  });
+});
